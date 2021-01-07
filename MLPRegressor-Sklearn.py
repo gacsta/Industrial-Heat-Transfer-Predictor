eaf94758b7e2ef2579132ae985580d1fc650d2c4
gacsta <gabr.871@gmail.com>
2021-01-07 14:55:25 -0300

    Added plotting feature, and data support from other .mat files.
    -Plotting Net prediction, y_test and X_test.
    -Score by MAE.
    -Loaded .mat (Temp_09-09-2019 and Temp_13-09-2019) files as dicitionary then dataframe (X and y, Input and Output)

--- MLPRegressor-Sklearn.py
+++ MLPRegressor-Sklearn.py
@@ -12,9 +12,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
 
 
-mat = scipy.io.loadmat('C:/Users/gabr8/Downloads/ML stuff/ML stuff/Recesso.Arquivos-20200321T191135Z-001/Recesso.Arquivos/Temp_09-09-2019 ruido 002.mat')
+#GET_DATA_FROM
+# from Sem_Ruido_Basico_sem_Parametros import *
+# X = Tind
+# y = Tproc
+
+mat = scipy.io.loadmat('C:/Users/gabr8/Downloads/ML stuff/ML stuff/Recesso.Arquivos-20200321T191135Z-001/Recesso.Arquivos/Temp_13-09-2019 ruido 002.mat')
 
 Inputs = np.concatenate((mat['TempSenoInput'].T, mat['TempQuadInput'].T, mat['TempSerInput'].T, mat['TempPadInput'].T, mat['TempTriInput'].T), axis = 0)
 Outputs = np.concatenate((mat['TempSenoOutput'].T, mat['TempQuadOutput'].T, mat['TempSerOutput'].T, mat['TempPadOutput'].T, mat['TempTriOutput'].T), axis = 0)
@@ -22,17 +28,19 @@ Outputs = np.concatenate((mat['TempSenoOutput'].T, mat['TempQuadOutput'].T, mat[
 X = pd.DataFrame(data = Inputs)
 y = pd.DataFrame(data = Outputs)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, test_size = 0.2, random_state = 0)
 
 
 
-regr = MLPRegressor(random_state=1, max_iter=100000, hidden_layer_sizes = (600)).fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.9, test_size = 0.1, random_state = 0)
+
+
+regr = MLPRegressor(random_state=np.random.randint(0,2000), max_iter=100000, hidden_layer_sizes = (500,), activation='identity').fit(X_train, y_train)
 Net = regr.predict(X_test[:])
-score = regr.score(X_test, y_test)
+Error = mean_absolute_error(y_test, Net)
 
 plt.figure()
-X_test.iloc[1, :].plot()
-X_train.iloc[1, :].plot()
-plt.plot(np.arange(0,1024), Net[1,:])
+# X_test.iloc[0, :].plot(color = 'r')
+y_test.iloc[8, :].plot(color = 'b')
+plt.plot(np.arange(0,1024), Net[0,:], 'g')
 
 
