40c6417a3bad3dc10bdfd282863e7d029c839704
gacsta <gabr.871@gmail.com>
2021-01-18 15:53:27 -0300

    Added Model Tester function
    2 Hidden Layer varying from specified range of neurons.
    Needs Cross Validation.
    Tind, Tproc, and Gama available for inputting.

--- MLPRegressor-Sklearn.py
+++ MLPRegressor-Sklearn.py
@@ -3,6 +3,7 @@
 Created on Mon Jan  4 16:06:34 2021
 
 @author: gabr8
+
 """
 
 import scipy.io
@@ -16,31 +17,60 @@ from sklearn.metrics import mean_absolute_error
 
 
 #GET_DATA_FROM
+
+from Tind_Tindn_Gama import *
+
 # from Sem_Ruido_Basico_sem_Parametros import *
 # X = Tind
 # y = Tproc
 
-mat = scipy.io.loadmat('C:/Users/gabr8/Downloads/ML stuff/ML stuff/Recesso.Arquivos-20200321T191135Z-001/Recesso.Arquivos/Temp_13-09-2019 ruido 002.mat')
+# mat = scipy.io.loadmat('C:/Users/gabr8/Downloads/ML stuff/ML stuff/Recesso.Arquivos-20200321T191135Z-001/Recesso.Arquivos/Temp_13-09-2019 ruido 002.mat')
 
-Inputs = np.concatenate((mat['TempSenoInput'].T, mat['TempQuadInput'].T, mat['TempSerInput'].T, mat['TempPadInput'].T, mat['TempTriInput'].T), axis = 0)
-Outputs = np.concatenate((mat['TempSenoOutput'].T, mat['TempQuadOutput'].T, mat['TempSerOutput'].T, mat['TempPadOutput'].T, mat['TempTriOutput'].T), axis = 0)
+# Inputs = np.concatenate((mat['TempSenoInput'].T, mat['TempQuadInput'].T, mat['TempSerInput'].T, mat['TempPadInput'].T, mat['TempTriInput'].T), axis = 0)
+# Outputs = np.concatenate((mat['TempSenoOutput'].T, mat['TempQuadOutput'].T, mat['TempSerOutput'].T, mat['TempPadOutput'].T, mat['TempTriOutput'].T), axis = 0)
 
-X = pd.DataFrame(data = Inputs)
-y = pd.DataFrame(data = Outputs)
-
-
+# X = pd.DataFrame(data = Inputs)
+# y = pd.DataFrame(data = Outputs)
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.9, test_size = 0.1, random_state = 0)
+def model_tester(X, y, hl):      
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 0)
+    
+    
+    regr = MLPRegressor(random_state=0, max_iter=100000, hidden_layer_sizes = hl).fit(X_train, y_train)
+    Net = regr.predict(X_test[:])
+    Error = mean_absolute_error(y_test, Net)
+    
+   
+    return Net, X_test, y_test, Error, hl
+
+#hidden layer sizes
+#500 --> 2000
+
+hl = np.arange(10, 1000, 20)
+modelos = []
+
+for i in hl:
+    for p in hl:
+        Net = model_tester(X, y, (i,p,))[0]
+        Tind = model_tester(X, y, (i,p,))[1]
+        Tproc = model_tester(X, y, (i,p,))[2]
+        Error = model_tester(X, y, (i,p,))[3]
+        
+        modelos.append(model_tester(X, y, (i,)))
+        
+        if Error < 0.4:
+            fig = plt.figure()
+            Tind.iloc[0, :].plot(color = 'r', label = 'Tind')
+            Tproc.iloc[17, :].plot(color = 'b', label = 'Tproc')
+            plt.plot(np.arange(0,1024), Net[0,:], 'g', label = 'Net')
+            title = 'Hidden Layer Sizes --> {}  {}  Error --> {}'.format(i, p,  Error)
+            fig.suptitle(title)
+   
 
 
-regr = MLPRegressor(random_state=np.random.randint(0,2000), max_iter=100000, hidden_layer_sizes = (500,), activation='identity').fit(X_train, y_train)
-Net = regr.predict(X_test[:])
-Error = mean_absolute_error(y_test, Net)
 
-plt.figure()
-# X_test.iloc[0, :].plot(color = 'r')
-y_test.iloc[8, :].plot(color = 'b')
-plt.plot(np.arange(0,1024), Net[0,:], 'g')
+
 
 
--- Tind_Tindn_Gama.py
+++ Tind_Tindn_Gama.py
@@ -4,20 +4,43 @@ Created on Sat Jan  9 21:30:30 2021
 
 @author: gabr8
 """
-
-
 #DATAFRAME FROM MULTIPLE LVM FILES 
 
 import pandas as pd
 from glob import glob
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
 
 semruido = sorted(glob('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_*.lvm'))
 
 #Merging files into one single dataframe
 #FIGURE OUT A WAY TO TRANSPOSE THE FEATURE COLUMNS 3 COLUMNS --->ONE ROW
-Inputs = pd.concat((pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= 0, header = None, names = ['Tproc', 'Tind', 'Trec', 'Tind-1', 'Tinf', 'Tau', 'Gama', 'Deltat']).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
-Inputs = Inputs.drop(columns = ['filename', 'Tproc', 'Tinf', 'Tau', 'Deltat', 'Trec'], axis = 0)
+
+#names = ['Tproc', 'Tind', 'Trec', 'Tind-1', 'Tinf', 'Tau', 'Gama', 'Deltat']
+
+Tind = pd.concat((pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [2]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
+Tind = Tind.drop(columns = ['filename'], axis = 0).T
+Tind.reset_index(inplace = True, drop =True)
+
+# Gama = pd.concat((pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [7]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
+# Gama = Gama.drop(columns = ['filename'], axis = 0).T
+# Gama.reset_index(inplace = True, drop =True)
+
+Tproc = pd.concat((pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [1]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
+Tproc = Tproc.drop(columns = ['filename'], axis = 0).T
+Tproc.reset_index(inplace = True, drop =True)
+
+# Trec = pd.concat((pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [3]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
+# Trec = Trec.drop(columns = ['filename'], axis = 0).T
+# Trec.reset_index(inplace = True, drop =True)
+
+
+# X = pd.concat([Gama, Tind], axis = 1)
+X = Tind
+y = Tproc
+
+
 
 
 # Tproc = pd.concat((pd.read_csv(file, encoding ='latin1', sep="\s+", index_col=None, usecols = ['Tproc']).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
@@ -34,4 +57,5 @@ Inputs = Inputs.drop(columns = ['filename', 'Tproc', 'Tinf', 'Tau', 'Deltat', 'T
 
 # test['Tproc'].plot(color = 'b')
 # test['Tind'].plot(color = 'g')
+
 
