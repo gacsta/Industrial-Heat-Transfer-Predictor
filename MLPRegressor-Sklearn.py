0695a84839bd3d5192247cabb850ae3118d4f072
gacsta <gabr.871@gmail.com>
2021-02-06 17:14:30 -0300

    Added Pipelines and K-fold cross validation
    Improved and organized validation process.

--- MLPRegressor-Sklearn.py
+++ MLPRegressor-Sklearn.py
@@ -1,72 +1,53 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan  4 16:06:34 2021
+Created on Tue Feb  2 18:05:23 2021
 
 @author: gabr8
-
 """
 
-import scipy.io
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPRegressor
-from sklearn.datasets import make_regression
-import matplotlib.pyplot as plt
-from sklearn.metrics import mean_absolute_error
 
+import pandas as pd
+import numpy as np
+
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+
+#Pipelines
+from sklearn.pipeline import Pipeline
 
 #GET_DATA_FROM
-
 from Tind_Tindn_Gama import *
 
-# from Sem_Ruido_Basico_sem_Parametros import *
-# X = Tind
-# y = Tproc
-
-# mat = scipy.io.loadmat('C:/Users/gabr8/Downloads/ML stuff/ML stuff/Recesso.Arquivos-20200321T191135Z-001/Recesso.Arquivos/Temp_13-09-2019 ruido 002.mat')
-
-# Inputs = np.concatenate((mat['TempSenoInput'].T, mat['TempQuadInput'].T, mat['TempSerInput'].T, mat['TempPadInput'].T, mat['TempTriInput'].T), axis = 0)
-# Outputs = np.concatenate((mat['TempSenoOutput'].T, mat['TempQuadOutput'].T, mat['TempSerOutput'].T, mat['TempPadOutput'].T, mat['TempTriOutput'].T), axis = 0)
-
-# X = pd.DataFrame(data = Inputs)
-# y = pd.DataFrame(data = Outputs)
+# #Splitting the data (Without K-fold cross val)
+# X_train, X_val, y_train, y_val = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 0)
 
 
+# ----> NEEDS ADAPTATION TO PIPELINE STRUCTURE <----
+#Prmimitive parameter testing
 def model_tester(X, y, hl):      
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 0)
+    #Generating pipeline
+    rede_pipeline = Pipeline(steps = [('mlp', MLPRegressor(random_state=0, max_iter=10000, hidden_layer_sizes = hl))])
     
+    #K-fold Cross Validation
+    Errors = -1*cross_val_score(rede_pipeline, X, y, cv = 5, scoring = 'neg_mean_absolute_error')
+    Mean_Error = Errors.mean()
+    Net = cross_val_predict(rede_pipeline, X,y, cv = 5)
     
-    regr = MLPRegressor(random_state=0, max_iter=100000, hidden_layer_sizes = hl).fit(X_train, y_train)
-    Net = regr.predict(X_test[:])
-    Error = mean_absolute_error(y_test, Net)
-    
-   
-    return Net, X_test, y_test, Error, hl
+    return Net, hl, Mean_Error
 
 #hidden layer sizes
 #500 --> 2000
-
-hl = np.arange(10, 100, 10)
-Best = []
+hl = np.arange(1000,1200, 50)
+Model = []
 
 for i in hl:
-    for p in hl:
-        
-        Error = model_tester(X, y, (i,p,))[3]
-        
-        if Error < 0.4:
-            #Save Net
-            Best.append(model_tester(X, y, (i,p)))
+
+            Model.append(model_tester(X, y, (i,)))
+                    
             
-            
-            # fig = plt.figure()
-            # Tind.iloc[0, :].plot(color = 'r', label = 'Tind')
-            # Tproc.iloc[17, :].plot(color = 'b', label = 'Tproc')
-            # plt.plot(np.arange(0,1024), Net[0,:], 'g', label = 'Net')
-            # title = 'Hidden Layer Sizes --> {}  {}  Error --> {}'.format(i, p,  Error)
-            # fig.suptitle(title)
    
 
 
--- Tind_Tindn_Gama.py
+++ Tind_Tindn_Gama.py
@@ -8,56 +8,22 @@ Created on Sat Jan  9 21:30:30 2021
 
 import pandas as pd
 from glob import glob
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-
-##MUDAR DIRETÓRIO
-
-semruido = sorted(glob('Sinais 05-11-2019-20210129T192834Z-001/Sinais 05-11-2019/Quadrada/quad_*.lvm'))
+sinais = sorted(glob('C:/Users/gabr8/OneDrive/Documentos/Machine Learning/Sinais 05-11-2019-20210129T192834Z-001/Sinais 05-11-2019/Quadrada/quad_*.lvm'))
 
 #Merging files into one single dataframe
-#FIGURE OUT A WAY TO TRANSPOSE THE FEATURE COLUMNS 3 COLUMNS --->ONE ROW
 
-#names = ['Tproc', 'Tind', 'Trec', 'Tind-1', 'Tinf', 'Tau', 'Gama', 'Deltat']
-
-Tind = pd.concat((pd.read_csv(file , encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [2]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
+Tind = pd.concat((pd.read_csv(file , encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [2]).assign(filename = file) for file in sinais), ignore_index=False, axis = 1)
 Tind = Tind.drop(columns = ['filename'], axis = 0).T
 Tind.reset_index(inplace = True, drop =True)
 
-# Gama = pd.concat((pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [7]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
-# Gama = Gama.drop(columns = ['filename'], axis = 0).T
-# Gama.reset_index(inplace = True, drop =True)
-
-Tproc = pd.concat((pd.read_csv(file, encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [1]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
+Tproc = pd.concat((pd.read_csv(file, encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [1]).assign(filename = file) for file in sinais), ignore_index=False, axis = 1)
 Tproc = Tproc.drop(columns = ['filename'], axis = 0).T
 Tproc.reset_index(inplace = True, drop =True)
 
-# Trec = pd.concat((pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [3]).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
-# Trec = Trec.drop(columns = ['filename'], axis = 0).T
-# Trec.reset_index(inplace = True, drop =True)
-
-
-# X = pd.concat([Gama, Tind], axis = 1)
 X = Tind
 y = Tproc
 
-
-
-
-# Tproc = pd.concat((pd.read_csv(file, encoding ='latin1', sep="\s+", index_col=None, usecols = ['Tproc']).assign(filename = file) for file in semruido), ignore_index=False, axis = 1)
-# Tproc = Tproc.drop(columns = ['filename'], axis = 0)
-# Tproc = Tproc.T
-
-
-# test = pd.read_csv('C:/Users/gabr8/Downloads/ML stuff/drive-download-20210108T200102Z-002/Sinais 05-11-2019/Quadrada/quad_3.lvm', encoding ='latin1', sep="\s+", index_col= 0, header = None, names = ['Tproc', 'Tind', 'Trec', 'Tind-1', 'Tinf', 'Tau', 'Gama', 'Deltat'] )    
-# features = ['Tind', 'Tind-1', 'Gama']
-# testX = test[features]
-# X = pd.concat([testX['Tind'], testX['Tind-1'], testX['Gama']], ignore_index= True, join = 'inner')
-
-# X = X.transpose()
-
-# test['Tproc'].plot(color = 'b')
-# test['Tind'].plot(color = 'g')
-
-
+#TEST SPLIT
+X, X_test, y , y_test = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 0)
