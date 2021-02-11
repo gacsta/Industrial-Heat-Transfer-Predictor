c38e936d2a125e726c058cede8f2254459e0fd48
gacsta <gabr.871@gmail.com>
2021-02-11 18:00:55 -0300

    Added Gama and Scaler
    Gama added to features, then properly scaledusing minmax from sklearn.

--- Tind_Tindn_Gama.py
+++ Tind_Tindn_Gama.py
@@ -10,20 +10,35 @@ import pandas as pd
 from glob import glob
 from sklearn.model_selection import train_test_split
 
+#datascaling
+from sklearn.preprocessing import MinMaxScaler
+
 sinais = sorted(glob('C:/Users/gabr8/OneDrive/Documentos/Machine Learning/Sinais 05-11-2019-20210129T192834Z-001/Sinais 05-11-2019/Quadrada/quad_*.lvm'))
 
 #Merging files into one single dataframe
 
 Tind = pd.concat((pd.read_csv(file , encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [2]).assign(filename = file) for file in sinais), ignore_index=False, axis = 1)
 Tind = Tind.drop(columns = ['filename'], axis = 0).T
-Tind.reset_index(inplace = True, drop =True)
 
 Tproc = pd.concat((pd.read_csv(file, encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [1]).assign(filename = file) for file in sinais), ignore_index=False, axis = 1)
 Tproc = Tproc.drop(columns = ['filename'], axis = 0).T
-Tproc.reset_index(inplace = True, drop =True)
 
-X = Tind
+Gama = pd.concat((pd.read_csv(file, encoding ='latin1', sep="\s+", index_col= None, header = None, usecols = [7]).assign(filename = file) for file in sinais), ignore_index=False, axis = 1)
+Gama = Gama.drop(columns = ['filename'], axis = 0).T
+
+Tind.reset_index(inplace = True, drop =True)
+Tproc.reset_index(inplace = True, drop =True)
+Gama.reset_index(inplace = True, drop =True)
+
+X = pd.concat([Tind, Gama], axis = 1, ignore_index=True)
 y = Tproc
+
+
+
+#SCALE DATA
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+y = scaler.fit_transform(y)
 
 #TEST SPLIT
 X, X_test, y , y_test = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 0)
